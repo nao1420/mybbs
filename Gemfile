50dea5d0adf40e8d886ea646e5e7f6b7a1357961
Cumene <toikon43@gmail.com>
2019-01-10 13:29:24 +0900

    hobo zenbu I18n

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'letter_opener_web'
+  gem "rails-erd"
+  #gem 'mailcatcher'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,16 @@ Rails.application.configure do
   end
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
+
+  #config.action_mailer.delivery_method = :letter_opener_web
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
